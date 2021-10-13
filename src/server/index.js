03287a7a283f78616152e8c044d7574781c6f218
sahil singh <sahil.singh@zeeve.io>
2021-10-13 11:10:57 +0530

    Init files

---
+++ src/server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const os = require('os');
+const cors = require('cors');
+
+const app = express();
+const corsOpts = {
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+
+app.use(cors(corsOpts));
+
+app.use(express.static('dist'));
+app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
